2754411c671d0f0f07c0681238969c24f0a04347
aankushh <ankushkhandelwal67@gmail.com>
2024-08-11 08:41:04 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/art-spaces/', // or '/' if deploying to the root
+  base: '/art-spaces/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
