424988aedbadbedc68bad945d5dba58728f523d5
aankushh <ankushkhandelwal67@gmail.com>
2024-08-11 08:26:59 +0530

    update base url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/art-spaces/',
+  base: '/dist',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
