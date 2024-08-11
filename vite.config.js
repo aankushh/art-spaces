8ecdd21e06b9b9c4dd7400a546f8cc5a1f05eb0f
aankushh <ankushkhandelwal67@gmail.com>
2024-08-11 08:34:26 +0530

    update repo base

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/art-spaces/', // or '/' if deploying to the root
   plugins: [vue(), vueDevTools()],
-  base: '/dist',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
